c9847d6a73616a3109365c2695bec8172fb35e48
AJEET SINGH <ajsin103@in.ibm.com>
2021-07-30 16:00:29 +0530

    Add Course Domain Model

---
+++ CleanArch/CleanArch.Domain/Models/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArch.Domain.Models
+{
+    class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string CourseDescription { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
